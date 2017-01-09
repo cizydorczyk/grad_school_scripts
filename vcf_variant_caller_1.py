eef15051ce0c38a0ccc669f12c9e3ae4602501f4
cizydorczyk <conradizydorczyk@gmail.com>
2017-01-09 11:12:17 -0500

    working on this script

--- vcf_variant_caller_1.py
+++ vcf_variant_caller_1.py
@@ -36,8 +36,8 @@ LQS_REF_CUTOFF = 0.80
 LQS_SNP_CUTOFF = 0.20
 
 
-#Define VcfVariants class;
-class VcfVariants(object):
+#Define VcfVariants class. Allows easy access to different fields of a variant.
+class VcfVariant(object):
     def __init__(self, isolateid, chrom, pos, ref, alt, quality, dp, dp4, filename):
         self.isolateid = isolateid
         self.chrom = chrom
@@ -49,56 +49,92 @@ class VcfVariants(object):
         self. dp4 = dp4
         self.filename = filename
 
+# Define vcf_parser function, which takes the input vcf file and outputs a list of VcfVariants
+# class objects:
+def vcf_parser(self):
+    f = open(filename, 'r')
+    var_list1 = []
+    variant_list = []
+    for line in f:
+        if not line.startswith("#"):
+            temp_list1 = []
+            temp_list2 = line.split()
+            temp_list1.append(temp_list2[0] + temp_list2[1])
+            temp_list1.append(temp_list2[0])
+            temp_list1.append(int(temp_list2[1]))
+            temp_list1.append(temp_list2[3])
+            temp_list1.append(temp_list2[4])
+            temp_list1.append(float(temp_list2[5]))
+            temp_list3 = temp_list2[7].split(";")
+            for item in temp_list3:
+                if item.startswith("DP="):
+                    if len(item) == 5:
+                        temp_list1.append(int(
+                            temp_list3[temp_list3.index(item)][-2] + temp_list3[temp_list3.index(item)][-1]))
+                    elif len(item) == 4:
+                        temp_list1.append(int(temp_list3[temp_list3.index(item)][-1]))
+                    elif len(item) == 6:
+                        temp_list1.append(int(temp_list3[temp_list3.index(item)][-3] + temp_list3[temp_list3.index(item)][-2] + temp_list3[temp_list3.index(item)][-1]))
+            for item in temp_list3:
+                if item.startswith("DP4"):
+                    temp_list4 = item[4:].split(",")
+                    temp_list5 = []
+                    for items in temp_list4:
+                        temp_list5.append(int(items))
+                    temp_list1.append(temp_list5)
+            temp_list1.append(filename)
+            var_list1.append(temp_list1)
+    # Call VcfVariant class on each parsed line (item in temp_list1)
+    for j in var_list1:
+        temp1 = VcfVariant(j[0], j[1], j[2], j[3], j[4], j[5], j[6], j[7], j[8])
+        variant_list.append(temp1)
+    f.close()
+    return variant_list
 
-# Define Parser class, which takes a list of file names (full paths); VcfVariant class is called on each individual
-# variant in each file
-class Parser(object):
-    def __init__(self, filename):
-        self.filename = filename
+variantlist = vcf_parser(filename)
 
-    def vcf_parser_multiple(self):
-        variant_dict = {}
-        f = open(self.filename, 'r')
-        var_list1 = []
-        variant_list = []
-        for line in f:
-            if not line.startswith("#"):
+# Create list of positions with indels
+def indel_pos_list(variantlist):
+    indel_pos_list = []
+    for variant in variantlist:
+        if len(variant.ref) > 1 or len(variant.alt) > 1:
+            if variant.pos not in indel_pos_list:
+                indel_pos_list.append(variant.pos)
+    return indel_pos_list
 
-                temp_list1 = []
-                temp_list2 = line.split()
-                temp_list1.append(temp_list2[0] + temp_list2[1])
-                temp_list1.append(temp_list2[0])
-                temp_list1.append(int(temp_list2[1]))
-                temp_list1.append(temp_list2[3])
-                temp_list1.append(temp_list2[4])
-                temp_list1.append(float(temp_list2[5]))
-                temp_list3 = temp_list2[7].split(";")
-                for item in temp_list3:
-                    if item.startswith("DP="):
-                        if len(item) == 5:
-                            temp_list1.append(int(
-                                temp_list3[temp_list3.index(item)][-2] + temp_list3[temp_list3.index(item)][-1]))
-                        elif len(item) == 4:
-                            temp_list1.append(int(temp_list3[temp_list3.index(item)][-1]))
-                        elif len(item) == 6:
-                            temp_list1.append(int(temp_list3[temp_list3.index(item)][-3] + temp_list3[temp_list3.index(item)][-2] + temp_list3[temp_list3.index(item)][-1]))
-                for item in temp_list3:
-                    if item.startswith("DP4"):
-                        temp_list4 = item[4:].split(",")
-                        temp_list5 = []
-                        for items in temp_list4:
-                            temp_list5.append(int(items))
-                        temp_list1.append(temp_list5)
-                temp_list1.append(self.filename)
-                var_list1.append(temp_list1)
-            # Call VcfVariant class on each parsed line (item in temp_list1)
-        for j in var_list1:
-            temp1 = VcfVariants(j[0], j[1], j[2], j[3], j[4], j[5], j[6], j[7], j[8])
-            variant_list.append(temp1)
+indel_list = indel_pos_list(variantlist)
 
-        variant_dict[self.filename] = variant_list
-        f.close()
-        return variant_dict
+# Used to sort high quality SNPs by position in hq_pos_list() function:
+def byPos_key(variant):
+    return variant.pos
 
-a = Parser(filename)
-b = a.vcf_parser_multiple()
+# Creates list of high quality SNP positions by filtering all SNPs using HQS criteria
+# at top of this file:
+def hq_pos_list(variantlist, indel_list):
+
+    hq_list = []
+    hq_pos = []
+    temp1 = []
+    for variant in variantlist:
+        if variant.dp >= HQS_DEPTH and variant.quality >= HQS_QUALITY and variant.dp4[2] >= HQS_ALT_THRESHOLD and variant.dp4[3] >= HQS_ALT_THRESHOLD and (variant.dp4[0] + variant.dp4[1]) < HQS_REF_MAX*(variant.dp4[2] + variant.dp4[3]) and variant.pos > HQS_MIN_DIST and variant.pos < (HQS_REF_LENGTH - HQS_MIN_DIST) and float(variant.dp4[2])/variant.dp4[3] > HQS_MIN_DP_PER and float(variant.dp4[3])/variant.dp4[2] > HQS_MIN_DP_PER and len(variant.ref) == 1 and len(variant.alt) == 1 and variant.pos not in itertools.chain.from_iterable([range(indel-HQS_MIN_DIST, indel + HQS_MIN_DIST + 1) for indel in indel_list]) and variant.pos not in hq_pos:
+            hq_pos.append(variant.pos)
+            temp1.append(variant)
+    hq_var = sorted(temp1, key=byPos_key)
+    hq_pos.sort()
+    hq_list.append(hq_pos)
+    hq_list.append(hq_var)
+    return hq_list
+
+hqposlist = hq_pos_list(variantlist, indel_list)
+
+# Based on high quality SNP positions, relax criteria for SNPs and identify any other SNPs with lower quality at THOSE positions:
+def lq_var_list(variantlist, indel_list, hqposlist):
+    lqvarlist = []
+
+    for variant in variantlist:
+        if variant.pos in hqposlist[0] and variant.dp >= LQS_DEPTH and variant.quality >= LQS_QUALITY and variant.dp4[2] >= LQS_ALT_THRESHOLD and variant.dp4[3] >= LQS_ALT_THRESHOLD and (variant.dp4[0] + variant.dp4[1]) < LQS_REF_MAX *(variant.dp4[2] + variant.dp4[3]) and variant.pos > LQS_MIN_DIST and variant.pos < (LQS_REF_LENGTH - LQS_MIN_DIST) and float(variant.dp4[2]) / variant.dp4[3] > LQS_MIN_DP_PER and float(variant.dp4[3]) / variant.dp4[2] > LQS_MIN_DP_PER and len(variant.ref) == 1 and len(variant.alt) == 1 and variant.pos not in itertools.chain.from_iterable([range(indel - LQS_MIN_DIST, indel + LQS_MIN_DIST + 1) for indel in indel_list]) and variant.pos not in lqvarlist:
+            lqvarlist.append(variant)
+    return lqvarlist
+
+
+lqvarlist = lq_var_list(variantlist, indel_list, hqposlist)
