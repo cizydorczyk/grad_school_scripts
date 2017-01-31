8f352d779772d200c3fab2726e5ba011041a9e6b
cizydorczyk <conradizydorczyk@gmail.com>
2017-01-31 15:54:57 -0500

    k

--- practice.py
+++ practice.py
@@ -1,48 +1,27 @@
 from sys import argv
-import itertools
-import pandas
+from oset import oset
+script, inputfile = argv
 
-script, inputblastfile, inputcontigstats, output_tsv = argv
+list1 = []
+with open(inputfile, 'r') as infile:
+    for line in infile:
+        if "orange" in line:
+            line1 = line.split("\t")
+            list1.append(line1[0])
 
+list2 = []
+for line in list1:
+    line2 = line.split("_")
+    list2.append(int(line2[-1]))
+list3 = range(1,900)
+print len(list3)
+print len(list2)
+set2 = oset(list2)
+set3 = oset(list3)
 
-def blast_parse(inputblastfile):
-    print "Working on file: " + str(inputblastfile)
-    data = pandas.read_table(inputcontigstats, sep="\t")
-    length_list = list(data["Consensus length"])
-    coverage_list = list(data["Average coverage"])
-    with open(inputblastfile, 'r') as infile:
-        lines = list(infile)
-    query_list = []
-    top_hit_list = []
-    fill_color = []
-    out_color = []
-    for lnum, i in enumerate(lines):
-        if "# Query: " in i:
-            if (lnum+4) < len(lines):
-                temp1 = i.split(' ')
-                query_list.append(temp1[-1].strip("\n"))
-                print "Query at index: " + str(lnum)
-                temp2 = lines[lnum+4]
-                temp3 = temp2.split("\t")
-                top_hit_list.append(temp3[-1].strip("\n"))
-                if "Pseudomonas aeruginosa" in temp3[-1]:
-                    fill_color.append("deepskyblue")
-                    out_color.append("blue")
-                else:
-                    fill_color.append("yellow")
-                    out_color.append("orange")
-            elif (lnum+4) >= len(lines):
-                print "Query at final index: " + str(lnum)
-                temp1 = i.split(' ')
-                query_list.append(temp1[-1].strip("\n"))
-                fill_color.append("yellow")
-                out_color.append("orange")
-                top_hit_list.append("N/A")
-    with open(output_tsv, 'w') as outfile:
-        for query, length, coverage, fill, out, hit in itertools.izip(query_list, length_list, coverage_list, fill_color, out_color, top_hit_list):
-            outfile.write(str(query) + "\t" + str(length) + "\t" + str(coverage) + "\t" + str(fill) + "\t" + str(out) + "\t" + str(hit) + "\n")
-
-
-
-
-contigs_to_remove = blast_parse(inputblastfile)
+count = 0
+for i in set3:
+    if i not in set2:
+        print i,
+        count += 1
+print "Total count is :" + str(count)
