69a8792fb741b11b47e53cc6f6ba119c12ee20d7
cizydorczyk <conradizydorczyk@gmail.com>
2017-04-29 13:46:00 -0400

    snp calling script'

--- practice2.py
+++ practice2.py
@@ -1,41 +1,24 @@
+"""
+for each high quality position:
+    for each isolate:
+        if an object with that position exists for the isolate:
+            test if SNP, REF, or N
+        elif an object with that position does not exist for the isolate:
+            record REF
+
+"""
+
 from sys import argv
-import itertools
-script, bwa6, novo6, last6 = argv
 
-def parse_vcf(bwafile, novofile, lastfile):
-    bwa_positions = []
-    novo_positions = []
-    last_positions = []
+script, bwa6, novo6 ,last6 = argv
 
-    with open(bwafile, 'r') as infile:
-        for line in infile:
-            if not line.startswith("#"):
-                bwa_positions.append(int(line.strip().split('\t')[1]))
-
-    with open(novofile, 'r') as infile:
-        for line in infile:
-            if not line.startswith("#"):
-                novo_positions.append(int(line.strip().split('\t')[1]))
-
-    with open(lastfile, 'r') as infile:
-        for line in infile:
-            if not line.startswith("#"):
-                last_positions.append(int(line.strip().split('\t')[1]))
-
-
-    hq_bwa_positions = set(bwa_positions)
-    hq_novo_positions = set(novo_positions)
-    hq_last_positions = set(last_positions)
-    hq_positions = (hq_bwa_positions & hq_novo_positions & hq_last_positions)
-
-    return hq_positions
-
-# Create list of common, quality 25 positions from 3 aligners
-positions = parse_vcf(bwa6, novo6, last6)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Define functions and classes
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # Define VcfVariants class. Allows easy access to different fields of a variant:
 class VcfVariant(object):
-    def __init__(self, chrom, pos, ref, alt, quality, dp, dp4, isolateid, variantid):
+    def __init__(self, chrom, pos, ref, alt, quality, dp, dp4, isolateid, variantid, caller):
         self.chrom = chrom
         self.pos = pos
         self.ref = ref
@@ -45,9 +28,12 @@ class VcfVariant(object):
         self.dp4 = dp4
         self.isolateid = isolateid
         self.variantid = variantid
+        self.caller = caller
+    def __repr__(self):
+        return str(self.caller)
 
-# Define function to prase VCF files and create VcfVariant class objects for each variant:
-def vcf_objects(vcf_file):
+# Define function to parse VCF files and create VcfVariant class objects for each variant:
+def parse_vcf(vcf_file):
     variant_objects_list_1 = []
     with open(vcf_file, 'r') as infile:
         for line in infile:
@@ -65,65 +51,44 @@ def vcf_objects(vcf_file):
                         temp1.append(item[4:].split(','))
                 temp1.append(vcf_file.split('/')[-1].split('_')[0])
                 temp1.append(temp1[-1]+ "_" + temp1[1])
-                variant_objects_list_1.append(VcfVariant(temp1[0], int(temp1[1]), temp1[2], temp1[3], float(temp1[4]), int(temp1[5]), temp1[6], temp1[7], temp1[8]))
+                temp1.append(vcf_file.split('/')[-1].split('_')[1].split('.')[0])
+                variant_objects_list_1.append(VcfVariant(temp1[0], int(temp1[1]), temp1[2], temp1[3], float(temp1[4]), int(temp1[5]), temp1[6], temp1[7], temp1[8], temp1[9]))
     return variant_objects_list_1
 
-# Create vcfvariant objects for each vcf file:
-bwa_objects = vcf_objects(bwa6)
-novo_objects = vcf_objects(novo6)
-last_objects = vcf_objects(last6)
+# Define class for variant list objects:
+class VariantListObject(object):
+    def __init__(self, variant_list):
+        self.variant_list = variant_list
 
-# Confirm that each vcf file for each aligner has a different amount of SNPs
-# print len(bwa_objects)
-# print len(novo_objects)
-# print len(last_objects)
+    def positions_list(self):
+        pos_list = []
+        for i in self.variant_list:
+            pos_list.append(i.pos)
+        return pos_list
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+bwa_objects = parse_vcf(bwa6)
+novo_objects = parse_vcf(novo6)
+last_objects = parse_vcf(last6)
+
+bwa_list_object = VariantListObject(bwa_objects)
+bwa_pos_list = bwa_list_object.positions_list()
+
+novo_list_object = VariantListObject(novo_objects)
+novo_pos_list = novo_list_object.positions_list()
+
+last_list_object = VariantListObject(last_objects)
+last_pos_list = last_list_object.positions_list()
+
+bwa_intersect = [i for i in bwa_objects if i.pos in novo_pos_list and i.pos in last_pos_list]
+print len(bwa_intersect)
+
+novo_intersect = [i for i in novo_objects if i.pos in bwa_pos_list and i.pos in last_pos_list]
+print len(novo_intersect)
+
+last_intersect = [i for i in last_objects if i.pos in bwa_pos_list and i.pos in novo_pos_list]
+print len(last_intersect)
 
 
-# For each list of vcf objects (one list per aligner), filter it and create a new list that contains only variants with positions found
-# in the high quality positions identified above:
-bwa_filtered_list = []
-for i in bwa_objects:
-    if i.pos in positions:
-        bwa_filtered_list.append(i)
-novo_filtered_list = []
-for i in novo_objects:
-    if i.pos in positions:
-        novo_filtered_list.append(i)
-last_filtered_list = []
-for i in last_objects:
-    if i.pos in positions:
-        last_filtered_list.append(i)
 
-
-## Confirm that each list, after filtering, contains the same positions in the same order
-for bwasnp, novosnp, lastsnp in itertools.izip(bwa_filtered_list, novo_filtered_list, last_filtered_list):
-    #print (int(bwasnp.dp4[0]) + int(bwasnp.dp4[1]) + int(bwasnp.dp4[2]) + int(bwasnp.dp4[3]))
-
-    if ((float(bwasnp.dp4[0]) + float(bwasnp.dp4[1])) / (float(bwasnp.dp4[0]) + float(bwasnp.dp4[1]) + float(bwasnp.dp4[2]) + float(bwasnp.dp4[3]))) >= 0.80 \
-    and ((float(novosnp.dp4[0]) + float(novosnp.dp4[1])) / (float(novosnp.dp4[0]) + float(novosnp.dp4[1]) + float(novosnp.dp4[2]) + float(novosnp.dp4[3]))) >= 0.80 \
-    and ((float(lastsnp.dp4[0]) + float(lastsnp.dp4[1])) / (float(lastsnp.dp4[0]) + float(lastsnp.dp4[1]) + float(lastsnp.dp4[2]) + float(lastsnp.dp4[3]))) >= 0.80:
-        print "REF!" + '\t' + str(bwasnp.pos)
-    elif ((float(bwasnp.dp4[0]) + float(bwasnp.dp4[1])) / (float(bwasnp.dp4[0]) + float(bwasnp.dp4[1]) + float(bwasnp.dp4[2]) + float(bwasnp.dp4[3]))) <= 0.20 \
-    and ((float(novosnp.dp4[0]) + float(novosnp.dp4[1])) / (float(novosnp.dp4[0]) + float(novosnp.dp4[1]) + float(novosnp.dp4[2]) + float(novosnp.dp4[3]))) <= 0.20 \
-    and ((float(lastsnp.dp4[0]) + float(lastsnp.dp4[1])) / (float(lastsnp.dp4[0]) + float(lastsnp.dp4[1]) + float(lastsnp.dp4[2]) + float(lastsnp.dp4[3]))) <= 0.20:
-        print "SNP!"+ '\t' + str(bwasnp.pos)
-    else:
-        print "AMBIGUOUS!"+ '\t' + str(bwasnp.pos)
-
-    # elif 0.20 < ((int(bwasnp.dp4[0]) + int(bwasnp.dp4[1])) / (int(bwasnp.dp4[0]) + int(bwasnp.dp4[1]) + int(bwasnp.dp4[2]) + int(bwasnp.dp4[3]))) < 0.80:
-    #     print "AMBIGUOUS!"
-    # else:
-    #     print "Positions do not match among the 3 aligners!"
-
-## Confirm that the length of each list is the same and equal to the length of common positions:
-
-# print bwa_filtered_list[-5].pos
-# print novo_filtered_list[-5].pos
-# print last_filtered_list[-5].pos
-
-# print len(bwa_filtered_list)
-# print len(novo_filtered_list)
-# print len(last_filtered_list)
-# print len(positions)
-
-## Code seems to work properly...
+#objects_intersect = [i for i in bwa_objects if i.pos in [j.pos for j in novo_objects] and i.pos in [k.pos for k in last_objects]]
+#print len(objects_intersect)
--- practice4.py
+++ practice4.py
@@ -1,16 +1,39 @@
 from sys import argv
-import numpy as np
-import pandas as pd
+import itertools
 
-script, bwafile, novofile, lastfile = argv
+script, bwa6, novo6 ,last6 = argv
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Define functions and classes
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Define function to get common positions from a set of vcf files and output it as a list:
+def get_common_positions(bwafile, novofile, lastfile):
+    bwa_positions = []
+    novo_positions = []
+    last_positions = []
+
+    with open(bwafile, 'r') as infile:
+        for line in infile:
+            if not line.startswith("#"):
+                bwa_positions.append(int(line.strip().split('\t')[1]))
+
+    with open(novofile, 'r') as infile:
+        for line in infile:
+            if not line.startswith("#"):
+                novo_positions.append(int(line.strip().split('\t')[1]))
+
+    with open(lastfile, 'r') as infile:
+        for line in infile:
+            if not line.startswith("#"):
+                last_positions.append(int(line.strip().split('\t')[1]))
+
+    hq_bwa_positions = set(bwa_positions)
+    hq_novo_positions = set(novo_positions)
+    hq_last_positions = set(last_positions)
+    hq_positions = (hq_bwa_positions & hq_novo_positions & hq_last_positions)
+
+    return hq_positions
 
 # Define VcfVariants class. Allows easy access to different fields of a variant:
 class VcfVariant(object):
-    def __init__(self, chrom, pos, ref, alt, quality, dp, dp4, isolateid, variantid, caller):
+    def __init__(self, chrom, pos, ref, alt, quality, dp, dp4, isolateid, variantid):
         self.chrom = chrom
         self.pos = pos
         self.ref = ref
@@ -20,9 +43,6 @@ class VcfVariant(object):
         self.dp4 = dp4
         self.isolateid = isolateid
         self.variantid = variantid
-        self.caller = caller
-    def __repr__(self):
-        return str(self.pos)
 
 # Define function to parse VCF files and create VcfVariant class objects for each variant:
 def parse_vcf(vcf_file):
@@ -43,67 +63,54 @@ def parse_vcf(vcf_file):
                         temp1.append(item[4:].split(','))
                 temp1.append(vcf_file.split('/')[-1].split('_')[0])
                 temp1.append(temp1[-1]+ "_" + temp1[1])
-                temp1.append(vcf_file.split('/')[-1].split('_')[1].split('.')[0])
-                variant_objects_list_1.append(VcfVariant(temp1[0], int(temp1[1]), temp1[2], temp1[3], float(temp1[4]), int(temp1[5]), temp1[6], temp1[7], temp1[8], temp1[9]))
+                variant_objects_list_1.append(VcfVariant(temp1[0], int(temp1[1]), temp1[2], temp1[3], float(temp1[4]), int(temp1[5]), temp1[6], temp1[7], temp1[8]))
     return variant_objects_list_1
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-bwa_objects = parse_vcf(bwafile)
-novo_objects = parse_vcf(novofile)
-last_objects = parse_vcf(lastfile)
+# Define function to get common vcf variants, based on common positions list:
+def get_common_variants(common_positions_list, variant_objects_list):
+    common_list = []
+    for i in variant_objects_list:
+        if i.pos in common_positions_list:
+            common_list.append(i)
+    return common_list
 
-variant_dict = {}
+# Define function to call snps:
+def call_snps(bwasnp, novosnp, lastsnp):
+    call = ''
+    if ((float(bwasnp.dp4[0]) + float(bwasnp.dp4[1])) / (float(bwasnp.dp4[0]) + float(bwasnp.dp4[1]) + float(bwasnp.dp4[2]) + float(bwasnp.dp4[3]))) >= 0.80 \
+    and ((float(novosnp.dp4[0]) + float(novosnp.dp4[1])) / (float(novosnp.dp4[0]) + float(novosnp.dp4[1]) + float(novosnp.dp4[2]) + float(novosnp.dp4[3]))) >= 0.80 \
+    and ((float(lastsnp.dp4[0]) + float(lastsnp.dp4[1])) / (float(lastsnp.dp4[0]) + float(lastsnp.dp4[1]) + float(lastsnp.dp4[2]) + float(lastsnp.dp4[3]))) >= 0.80:
+        call = bwasnp.ref
+    elif ((float(bwasnp.dp4[0]) + float(bwasnp.dp4[1])) / (float(bwasnp.dp4[0]) + float(bwasnp.dp4[1]) + float(bwasnp.dp4[2]) + float(bwasnp.dp4[3]))) <= 0.20 \
+    and ((float(novosnp.dp4[0]) + float(novosnp.dp4[1])) / (float(novosnp.dp4[0]) + float(novosnp.dp4[1]) + float(novosnp.dp4[2]) + float(novosnp.dp4[3]))) <= 0.20 \
+    and ((float(lastsnp.dp4[0]) + float(lastsnp.dp4[1])) / (float(lastsnp.dp4[0]) + float(lastsnp.dp4[1]) + float(lastsnp.dp4[2]) + float(lastsnp.dp4[3]))) <= 0.20:
+        call = bwasnp.alt
+    else:
+        call = 'N'
+    return call
 
-for i in bwa_objects:
-    if i.pos not in variant_dict:
-        if ((float(i.dp4[0]) + float(i.dp4[1])) / (float(i.dp4[0]) + float(i.dp4[1]) + float(i.dp4[2]) + float(i.dp4[3]))) >= 0.80:
-            variant_dict[i.pos] = ['REF']
-        elif ((float(i.dp4[0]) + float(i.dp4[1])) / (float(i.dp4[0]) + float(i.dp4[1]) + float(i.dp4[2]) + float(i.dp4[3]))) <= 0.20:
-            variant_dict[i.pos] = ['SNP']
-        else:
-            variant_dict[i.pos] = ['N']
-for i in novo_objects:
-    if i.pos not in variant_dict:
-        if ((float(i.dp4[0]) + float(i.dp4[1])) / (float(i.dp4[0]) + float(i.dp4[1]) + float(i.dp4[2]) + float(i.dp4[3]))) >= 0.80:
-            variant_dict[i.pos] = [np.nan, 'REF']
-        elif ((float(i.dp4[0]) + float(i.dp4[1])) / (float(i.dp4[0]) + float(i.dp4[1]) + float(i.dp4[2]) + float(i.dp4[3]))) <= 0.20:
-            variant_dict[i.pos] = [np.nan, 'SNP']
-        else:
-            variant_dict[i.pos] = [np.nan, 'N']
-    elif i.pos in variant_dict:
-        if ((float(i.dp4[0]) + float(i.dp4[1])) / (float(i.dp4[0]) + float(i.dp4[1]) + float(i.dp4[2]) + float(i.dp4[3]))) >= 0.80:
-            variant_dict[i.pos].append('REF')
-        elif ((float(i.dp4[0]) + float(i.dp4[1])) / (float(i.dp4[0]) + float(i.dp4[1]) + float(i.dp4[2]) + float(i.dp4[3]))) <= 0.20:
-            variant_dict[i.pos].append('SNP')
-        else:
-            variant_dict[i.pos].append('N')
-for i in last_objects:
-    if i.pos not in variant_dict:
-        if ((float(i.dp4[0]) + float(i.dp4[1])) / (float(i.dp4[0]) + float(i.dp4[1]) + float(i.dp4[2]) + float(i.dp4[3]))) >= 0.80:
-            variant_dict[i.pos] = [np.nan, 'REF']
-        elif ((float(i.dp4[0]) + float(i.dp4[1])) / (float(i.dp4[0]) + float(i.dp4[1]) + float(i.dp4[2]) + float(i.dp4[3]))) <= 0.20:
-            variant_dict[i.pos] = [np.nan, 'SNP']
-        else:
-            variant_dict[i.pos] = [np.nan, 'N']
-    elif i.pos in variant_dict:
-        if ((float(i.dp4[0]) + float(i.dp4[1])) / (float(i.dp4[0]) + float(i.dp4[1]) + float(i.dp4[2]) + float(i.dp4[3]))) >= 0.80:
-            variant_dict[i.pos].append('REF')
-        elif ((float(i.dp4[0]) + float(i.dp4[1])) / (float(i.dp4[0]) + float(i.dp4[1]) + float(i.dp4[2]) + float(i.dp4[3]))) <= 0.20:
-            variant_dict[i.pos].append('SNP')
-        else:
-            variant_dict[i.pos].append('N')
+# Identify common positions between 3 aligners:
+common_positions_25 = get_common_positions(bwa6, novo6, last6)
 
-for key in variant_dict:
-    if len(variant_dict[key]) == 2:
-        variant_dict[key].append(np.nan)
-    elif len(variant_dict[key]) == 1:
-        variant_dict[key].append(np.nan)
-        variant_dict[key].append(np.nan)
+# Create lists of variant objects for each aligner:
+bwa_objects = parse_vcf(bwa6)
+novo_objects = parse_vcf(novo6)
+last_objects = parse_vcf(last6)
 
-vardf = pd.DataFrame.from_dict(variant_dict, orient='index')
-vardf.sort_index(inplace=True)
-vardf.columns = ['bwa', 'novo', 'last']
-vardf.dropna(axis=0, inplace=True)
+# Identify variant objects that have positions in the common positions list created above:
+bwa_filtered_list = get_common_variants(common_positions_25, bwa_objects)
+novo_filtered_list = get_common_variants(common_positions_25, novo_objects)
+last_filtered_list = get_common_variants(common_positions_25, last_objects)
+
+# Call SNPs for each set of 3 objects in filtered lists:
+dictt = {}
+for bwasnp, novosnp, lastsnp in itertools.izip(bwa_filtered_list, novo_filtered_list, last_filtered_list):
+    dictt[bwasnp.pos] = [bwasnp.ref, call_snps(bwasnp, novosnp, lastsnp)]
+
+print dictt
 
 
-print vardf.iloc[i]
+
+
+#for i in bwa_filtered_list:
+#    print str(i.pos) + '\t' + str(i.ref) + '\t' + str(i.alt)
